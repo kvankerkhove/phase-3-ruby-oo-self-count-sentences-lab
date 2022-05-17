9d57538e46c5b9e82c3125c5dbd68b1370623bd1
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-05-17 16:27:28 -0400

    commit tree b9a540b861f6538735506c352a5dabfbdd782850

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter{|sentence| !sentence.empty?}.length
   end
 end
+
+puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
